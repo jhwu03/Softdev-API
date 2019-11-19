acdfa908c01fb4a483661297a6bc844ff117e321
phuang00 <phuang00@stuy.edu>
2019-11-19 09:24:13 -0500

    added python files for handling database

---
+++ utl/db_manager.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+def add_login(username, password):
+    return ""
+
+def verify_login(username, password):
+    return ""
+
+def convert_currency(curr_1, value, curr_2):
+    return ""
+
+def reset_quiz():
+    return ""
+
+def get_name_stats(name, country):
+    return ""
+
+def has_name(name, country):
+    return ""
+
+def add_name(name, country, count, age):
+    return ""
+
+def get_alpha_2(country):
+    return ""
+
+def get_alpha_3(country):
+    return ""
+
+def has_currency(country_1, country_2):
+    return ""
+
+def add_currency(currency_1, currency_2, rate):
+    return ""
+
+def has_country(country):
+    return ""
+
+def add_country(country, calling_code, cap, pop, lang, flag, curr, reg):
+    return ""
+
+def search_country(keyword):
+    return ""
