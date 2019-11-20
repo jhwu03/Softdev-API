24862dd785c59f59e5c6bc24bf9b96ab09e43e38
phuang00 <phuang00@stuy.edu>
2019-11-19 20:59:27 -0500

    worked on database functions and updated dev log

--- utl/db_manager.py
+++ utl/db_manager.py
@@ -1,10 +1,36 @@
 import sqlite3
 
+DB_FILE = "database.db"
+
+def close_db(database):
+    database.commit()
+    database.close()
+
 def add_login(username, password):
-    return ""
+    if username == "" or password == "":
+        return "Credentials cannot be left blank"
+    database = sqlite3.connect(DB_FILE)
+    cur = database.cursor()
+    message = ""
+    cur.execute("SELECT * FROM users WHERE user_name = ?;", (username,))
+    if cur.fetchone() is None:
+        cur.execute("INSERT INTO users(user_name, user_password) VALUES(?, ?);",
+                    (username, password,))
+    else:
+        message = "Username already exists!"
+    close_db(database)
+    return message
 
 def verify_login(username, password):
-    return ""
+    if username == "" or password == "":
+        return "Credentials cannot be left blank"
+    database = sqlite3.connect(DB_FILE)
+    cur = database.cursor()
+    message = ""
+    cur.execute("SELECT * FROM users WHERE user_name = ? AND user_password = ?;" , (username, password,))
+    if cur.fetchone() is None:
+        message = "Login credentials not found! Please try again."
+    return message
 
 def convert_currency(curr_1, value, curr_2):
     return ""
